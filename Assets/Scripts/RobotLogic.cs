2954c00e4974db901792902b6bf19592df48acd3
yvens <yvensre@gmail.com>
2020-02-01 01:43:56 +0100

    Added animations to the robots and changed the scenario

---
+++ Assets/Scripts/RobotLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private Vector2 animationSpeedVariation;
+
+    void Awake() {
+        animator.SetFloat("RobotAnimationSpeed", Random.Range(animationSpeedVariation.x, animationSpeedVariation.y));
+    }
+}
