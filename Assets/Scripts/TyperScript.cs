0678ee9511571c7569b8007c30dda3e2d7110cdc
yvens <yvensre@gmail.com>
2020-02-02 02:24:32 +0100

    Added text and animation in the title screen Started to make the scenario for the game a bit more interesting

--- Assets/Scripts/TyperScript.cs
+++ Assets/Scripts/TyperScript.cs
@@ -64,7 +64,6 @@ public class TyperScript : MonoBehaviour
         timer = Mathf.Max(0.0f, timer - value);
         if(timer == 0.0f) {
             charStep++;
-            // timer = initialTimer;
         }
     }
 
